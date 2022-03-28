06e9ad2460b99af15030673acf484fc990621beb
scn-tech <scnaikwadi2@gmail.com>
2022-03-28 08:26:25 +0530

    Collection ; Set Interface
    Collection ; Set Interface

--- code/corejava/workspace/Collection/src/HashCodeEquals.java
+++ code/corejava/workspace/Collection/src/HashCodeEquals.java
@@ -1,19 +1,23 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class HashCodeEquals {
 	public static void main(String[] args) {
-		Student s1 = new Student("A", "9900998899");
-		Student s2 = new Student("B", "9900998877");
-		Student s3 = new Student("C", "9901998899");
-		Student s4 = new Student("D", "9900998899");
-		Student s5 = new Student("E", "9922998899");
+		Student s1 = new Student("A", "9900998899", 345);
+		Student s2 = new Student("B", "9900998877", 324);
+		Student s3 = new Student("C", "9901998899", 654);
+		Student s4 = new Student("D", "9900998899", 123);
+		Student s5 = new Student("E", "9922998899", 247);
+		Student s6 = new Student("F", "772998899", 247);
 
 		HashSet set = new HashSet();
+		//LinkedHashSet set = new LinkedHashSet();
 		set.add(s1);
 		set.add(s2);
 		set.add(s3);
 		set.add(s4);
 		set.add(s5);
+		set.add(s6);
 
 		System.out.println(set);
 
@@ -23,26 +27,47 @@ public class HashCodeEquals {
 class Student {
 	String name;
 	String contact;
+	int id;
 
-	public Student(String name, String contact) {
+	public Student(String name, String contact, int id) {
 		this.name = name;
 		this.contact = contact;
+		this.id = id;
 	}
-
+	
+	
 	@Override
 	public String toString() {
-		return "Name : " + name + "  Contact : " + contact + "\n";
+		return "name=" + name + ", contact=" + contact + ", id=" + id + "\n";
 	}
 
 	@Override
 	public int hashCode() {
-		return contact.length();
+		return id;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
-		Student st = (Student) obj;
-		return contact.equals(st.contact);
+		Student st = (Student) obj; 
+		if(id == st.id) {
+			return true;
+		}else {
+			return false;
+		}
+		
+		//return id == st.id;
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/corejava/workspace/Collection/src/TreeSetDemo.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		TreeSet set = new TreeSet();
+		set.add(546);
+		set.add(423);
+		set.add(134);
+		set.add(765);
+		set.add(765);
+		set.add(987);
+		set.add(121);
+		
+		System.out.println(set);
+		
+		System.out.println("Smallest : "+set.first());
+		System.out.println("Largest : "+set.last());
+		
+		// grater value than the provided value
+		System.out.println("Higher : "+set.higher(520)); // 546
+		
+		// smaller value than the provided value
+		System.out.println("Lower : "+set.lower(520)); // 423
+		
+		// equals or grater value than the provided value
+		System.out.println("ceiling : "+set.ceiling(520)); // 546
+		
+		// equals or less value than the provided value
+		System.out.println("floor : "+set.floor(520)); // 423
+		
+		
+		System.out.println("=================");
+		Iterator it = set.iterator();
+		//it.hasNext(); // it check for the next method present or not.
+		//it.next(); // it jumps on the next value and also return the value.
+		
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+	}
+
+}
