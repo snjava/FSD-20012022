25067e30363311765541a77d953e374995f2eed8
snjava <scnaikwadi2@gmail.com>
2022-06-08 11:13:38 +0530

    Spring JDBC and Swagger Docs
    Spring JDBC and Swagger Docs

--- code/springboot/sb-jdbc/src/main/java/org/institute/controller/PhBookController.java
+++ code/springboot/sb-jdbc/src/main/java/org/institute/controller/PhBookController.java
@@ -1,13 +1,17 @@
 package org.institute.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.institute.dto.PhBookDto;
 import org.institute.repository.PhBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +34,30 @@ public class PhBookController {
 	@GetMapping("contact/get-all")
 	public List getAllContact() {
 		return repository.getAllContact();
+	}
+	
+	@DeleteMapping("/contact/delete-by-id")
+	public String deleteContact(@RequestParam("id") int id) {
+		int result = repository.deleteContact(id);
+		if(result > 0) {
+			return "Contact Deleted Successfully";
+		} else {
+			return "Contact Deletion Failed.. Please try after sometime.";
+		}
+	}
+	
+	@PutMapping("/contact/update")
+	public String updateContact(@RequestBody PhBookDto phBookDto) {
+		int result = repository.updateContact(phBookDto);
+		return (result > 0) ? 
+					"Contact Updated Successfully" 
+				: 
+					"Contact Updation Failed.. Please try after sometime.";
+	}
+	
+	@GetMapping("/contact/get-by-id")
+	public Map getContactById(@RequestParam("id") int id) {
+		return repository.getContactById(id);
 	}
 	
 }
--- code/springboot/sb-jdbc/src/main/java/org/institute/repository/PhBookRepository.java
+++ code/springboot/sb-jdbc/src/main/java/org/institute/repository/PhBookRepository.java
@@ -1,6 +1,7 @@
 package org.institute.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.institute.dto.PhBookDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,37 +13,42 @@ public class PhBookRepository {
 
 	@Autowired
 	private JdbcTemplate template;
-	
+
 	public int saveContact(PhBookDto dto) {
 		String query = "insert into phbook values(0,?,?,?)";
-		//int count = template.update(query, dto.getFname(), dto.getLname(), dto.getContact());
-		
-		Object data[] = {dto.getFname(), dto.getLname(), dto.getContact()};
+		// int count = template.update(query, dto.getFname(), dto.getLname(),
+		// dto.getContact());
+
+		Object data[] = { dto.getFname(), dto.getLname(), dto.getContact() };
 		int count = template.update(query, data);
 		return count;
 	}
-	
+
+	// update the contact number by using ID
+	public int updateContact(PhBookDto dto) {
+		String query = "update phbook set contact=? where id=?";
+		Object data[] = { dto.getContact(), dto.getId() };
+		int count = template.update(query, data);
+		return count;
+	}
+
+	// Delete the contact using ID
+	public int deleteContact(int id) {
+		String query = "delete from phbook where id=?";
+		int count = template.update(query, id);
+		return count;
+	}
 	
 	public List getAllContact() {
 		String query = "select * from phbook";
-		List records =  template.queryForList(query);
+		List records = template.queryForList(query);
 		return records;
 	}
 	
-	
-	// update the contact number by using ID
-	
-	// Delete the contact using ID
-	
+	public Map getContactById(int id) {
+		String query = "select * from phbook where id = ?";
+		Object data[] = {id};
+		Map record = template.queryForMap(query,data);
+		return record;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
